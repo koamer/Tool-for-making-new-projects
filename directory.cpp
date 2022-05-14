d8f45db58d8e0c26d02eaced29e08a6eb4ea6903
koamer <tom9807@wp.pl>
2022-05-14 15:10:29 +0200

    Adding proper logs

--- directory.cpp
+++ directory.cpp
@@ -5,13 +5,14 @@
 #include <cstdlib>
 #include <curses.h>
 
-void directory::directory_menu() {
-    DIR* current_directory = opendir("~");
-    if(current_directory == NULL) {
-        fprintf(stderr, 
-            "Cannot open home directory. Check perrmisions");
+void directory::directory_menu()
+{
+    DIR *current_directory = opendir("~");
+    if (current_directory == NULL)
+    {
+        fprintf(stderr,
+                "Cannot open home directory. Check perrmisions");
         endwin();
         exit(EXIT_FAILURE);
     }
-    
 }
--- main.cpp
+++ main.cpp
@@ -10,17 +10,19 @@
 #include <sys/types.h>
 #include <ctime>
 #include <chrono>
+#include <clocale>
 
 #include "directory.hpp"
 
-bool create_directory(void) {
-	directory::directory_menu();	
-
+bool create_directory(void)
+{
+	// directory::directory_menu();
 
 	return true;
 }
 
-enum class Options {
+enum class Options
+{
 	OPTIONS_1 = 1,
 	OPTIONS_2 = 2,
 	OPTIONS_3 = 3,
@@ -28,122 +30,175 @@ enum class Options {
 	EXIT = 5,
 };
 
-const std::string get_current_time(void) {
+const std::string get_current_time(void)
+{
+	char time_string[64];
 	auto time = std::chrono::system_clock::now();
-	//TODO
+	auto time_c = std::chrono::system_clock::to_time_t(time);
+	std::tm now_tm = *std::localtime(&time_c);
+	strftime(time_string, sizeof(time_string), "%A %c", &now_tm);
+	return (std::string(time_string));
 }
 
-bool init_logs_1_stage(void) {
-	int fd = open("logs.txt", O_CREAT | O_WRONLY, S_IRWXU);
-	if (dup2(fd, STDERR_FILENO) < 0) {
+static void init_logs_1_stage(void)
+{
+	int fd_error_logs = open("err_logs.txt", O_CREAT | O_WRONLY, S_IRWXU);
+	if (fd_error_logs < 0)
+	{
 		endwin();
-		printf("Cannot attach error output to logs file\n");
+		printf("Cannot create error logs file\n");
 		exit(EXIT_FAILURE);
 	}
-	const std::string time = get_current_time();
-	fprintf(stderr, "LOGS %s : %s", __DATE__, time.c_str());
-	return true;
+	if (dup2(fd_error_logs, STDERR_FILENO) < 0)
+	{
+		endwin();
+		printf("Cannot attach to error logs file\n");
+		exit(EXIT_FAILURE);
+	}
+	int fd_debug_logs = open("debug_logs.txt", O_CREAT | O_WRONLY, S_IRWXU);
+	if (fd_debug_logs < 0)
+	{
+		endwin();
+		printf("Cannot create debug logs file\n");
+		exit(EXIT_FAILURE);
+	}
+	if (dup2(fd_debug_logs, STDOUT_FILENO) < 0)
+	{
+		endwin();
+		printf("Cannot attach to debug logs file\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
-inline void init_ncurse(void) {
-    initscr();
+inline void logs_err(std::string logs)
+{
+	std::string time = get_current_time();
+	fprintf(stderr, "Logs %s: ERROR: %s\n", time.c_str(), logs.c_str());
+}
+
+inline void logs_debug(std::string logs)
+{
+	std::string time = get_current_time();
+	fprintf(stdout, "Logs %s: DEBUG: %s\n", time.c_str(), logs.c_str());
+}
+
+inline void init_ncurse(void)
+{
+	initscr();
 	cbreak();
-	noecho();   
+	noecho();
 	keypad(stdscr, TRUE);
 	init_logs_1_stage();
 }
 
-Options menu(void) {
+static Options menu(void)
+{
 	enum Options opt = Options::EXIT;
-    ITEM  **my_items;
-	MENU *my_menu; 
+	ITEM **my_items;
+	MENU *my_menu;
 	std::vector<std::pair<std::string, std::string>> choices = {
-		{"1. C++", "Creating C++ enviroment"},
-		{"2. C", "Creating C enviroment"},
-		{"3. Python", "Crearing Python source file"},
-		{"4. Shell Script", "Creating Shell script"},
-		{"5. Exit", ""},
+			{"1. C++", "Creating C++ enviroment"},
+			{"2. C", "Creating C enviroment"},
+			{"3. Python", "Crearing Python source file"},
+			{"4. Shell Script", "Creating Shell script"},
+			{"5. Exit", ""},
 	};
-    int num_of_choices = choices.size();
-    my_items = (ITEM **)calloc(num_of_choices + 1, sizeof(ITEM *));	
-	for(size_t i = 0; i < choices.size(); i++) {
-	    my_items[i] = new_item(choices[i].first.c_str(),
-					  choices[i].second.c_str());
-    }
+	int num_of_choices = choices.size();
+	my_items = (ITEM **)calloc(num_of_choices + 1, sizeof(ITEM *));
+	for (size_t i = 0; i < choices.size(); i++)
+	{
+		my_items[i] = new_item(choices[i].first.c_str(),
+													 choices[i].second.c_str());
+	}
 	my_items[num_of_choices] = (ITEM *)NULL;
 
-    my_menu = new_menu((ITEM **)my_items);
+	my_menu = new_menu((ITEM **)my_items);
 	post_menu(my_menu);
 	refresh();
 
 	int c;
 	unsigned int current_options;
-	while((c = getch()))
-	{   switch(c)
-	    {	
-			case KEY_DOWN: {
-		        menu_driver(my_menu, REQ_DOWN_ITEM);
-				break;
-			}
-			case KEY_UP: {
-				menu_driver(my_menu, REQ_UP_ITEM);
-				break;
-			}
-			case 10: /* ENTER KEY */{
-				move(20, 0);
-				char selected_item = item_name(current_item(my_menu))[0];
-				current_options = static_cast<unsigned int>(selected_item - '0');
-				opt = static_cast<Options>(current_options);
-				break;
-			}
-		}	
+	while ((c = getch()))
+	{
+		switch (c)
+		{
+		case KEY_DOWN:
+		{
+			menu_driver(my_menu, REQ_DOWN_ITEM);
+			break;
+		}
+		case KEY_UP:
+		{
+			menu_driver(my_menu, REQ_UP_ITEM);
+			break;
+		}
+		case 10: /* ENTER KEY */
+		{
+			move(20, 0);
+			char selected_item = item_name(current_item(my_menu))[0];
+			current_options = static_cast<unsigned int>(selected_item - '0');
+			opt = static_cast<Options>(current_options);
+			break;
+		}
+		}
 	}
-	return opt;	
+	return opt;
 }
-void cpp_menu() {
-	//TODO
+void cpp_menu()
+{
+	// TODO
 }
-void c_menu() {
-	//TODO
+void c_menu()
+{
+	// TODO
 }
-void sh_menu() {
-	//TODO
+void sh_menu()
+{
+	// TODO
 }
-void pyhon_menu() {
-	//TODO
+void pyhon_menu()
+{
+	// TODO
 }
 
 int main(void)
-{	
-    init_ncurse();
-    Options choice = menu();
-	switch(choice) {
-		case Options::OPTIONS_1: {
-			cpp_menu();
-			break;
-		}
-		case Options::OPTIONS_2: {
-			c_menu();
-			break;
-		}
-		case Options::OPTIONS_3: {
-			pyhon_menu();
-			break;
-		}
-		case Options::OPTIONS_4: {
-			sh_menu();
-			break;
-		}
-		case Options::EXIT: {
-			endwin();
-			exit(EXIT_SUCCESS);
-			break;
-		}
-		default: {
-			endwin();
-			exit(EXIT_FAILURE);
-			printw("Something went wrong");
-		}
+{
+	init_ncurse();
+	Options choice = menu();
+	switch (choice)
+	{
+	case Options::OPTIONS_1:
+	{
+		cpp_menu();
+		break;
 	}
-    endwin();
+	case Options::OPTIONS_2:
+	{
+		c_menu();
+		break;
+	}
+	case Options::OPTIONS_3:
+	{
+		pyhon_menu();
+		break;
+	}
+	case Options::OPTIONS_4:
+	{
+		sh_menu();
+		break;
+	}
+	case Options::EXIT:
+	{
+		endwin();
+		exit(EXIT_SUCCESS);
+		break;
+	}
+	default:
+	{
+		endwin();
+		exit(EXIT_FAILURE);
+		printw("Something went wrong");
+	}
+	}
+	endwin();
 }
